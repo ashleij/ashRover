9500f92ff8a0a05e6e112643e56a21e576547792
ashlei <ashlei.jones@detroitlabs.com>
2017-05-22 15:40:17 -0400

    Adding a cancellation message if rover is given no commands. Adding arrays for X and Y values. Adding a function to check for out-of-bounds index when assigning coordinate in order to wrap planet.

--- ashRover.js
+++ ashRover.js
@@ -12,16 +12,49 @@ var cordX;
 var cordY;
 var pos;
 var commandArray = [];
+var XcoordinatePosArr = [];
+var YcoordinatePosArr = [];
 
 //Formats the coordinates as '[x,y]'.
 function roverPosition(x,y) {
 	return "["+x+","+y+"]";
 }
 
+//Checks the Y coordinate to make sure it is not out of bounds (wrapping planet).
+function checkIndexY(index) {
+	if (index === 21) {
+		indexY -= 21;
+		return 0;
+	} if (index === -1) {
+		indexY += 21;
+		return 20;
+	} else {
+		return index;
+	}
+}
+//Checks the X coordinate to make sure it is not out of bounds (wrapping planet).
+function checkIndexX(index) {
+	if (index === 21) {
+		indexX -= 21;
+		return 0;
+	} if (index === -1) {
+		indexX += 21;
+		return 20;
+	} else {
+		return index;
+	}
+}
+
 //Starts the app, relays starting position.
-function roverStart() {
-	cordX = 0;
-	cordY = 0;
+function roverStart(){
+	for (var i = -10; i <= 10; i++) {
+		XcoordinatePosArr.push(i);
+		YcoordinatePosArr.push(i);
+	}
+	indexX = 10;
+	indexY = 10;
+	cordX = XcoordinatePosArr[indexX];
+	cordY = YcoordinatePosArr[indexY];
 	pos = 'N';
 	console.log("Your starting position is " + roverPosition(cordX,cordY) + ". You are facing " + pos + ".");
 	getCommands();
@@ -29,7 +62,6 @@ function roverStart() {
 
 //Handles the user's input for the rover's commands.
 function getCommands() {
-	console.log(commandArray);
 	prompt.question("Enter your commands ('L' or 'R' to turn. 'F 'or 'B' to move. 'C' to finish entering commands.): ", (inputCommands) => {
 		var inputCommandsUC = inputCommands.toUpperCase();
 		if (inputCommandsUC == "L" || inputCommandsUC == "R" || inputCommandsUC == "F" || inputCommandsUC == "B") {
@@ -62,8 +94,8 @@ function analyzeCommand(x) {
 			pos = "N";
 		}
 		return 'The rover turned left. It is now facing ' + pos + ".";
+	//Rover turns right. Changes direction.
 	} else if (x == "R") {
-		//Rover turns right. Changes direction.
 		if (pos == "N") {
 			pos = "E";
 		} else if (pos == "E") {
@@ -74,40 +106,54 @@ function analyzeCommand(x) {
 			pos = "N";
 		}
 		return 'The rover turned right. It is now facing ' + pos + ".";
+	//Rover moves forward. Changes coordinate.
 	} else if (x == "F") {
-		//Rover moves forward. Changes coordinate.
 		if (pos == "N") {
-			cordY += 1;
+			indexY++;
+			cordY = YcoordinatePosArr[checkIndexY(indexY)];
 		} else if (pos == "S") {
-			cordY -= 1;
+			indexY--;
+			cordY = YcoordinatePosArr[checkIndexY(indexY)];
 		} else if (pos == "E") {
-			cordX += 1;
+			indexX++;
+			cordX = XcoordinatePosArr[checkIndexX(indexX)];
 		} else if (pos == "W") {
-			cordX -= 1;
+			indexX--;
+			cordX = XcoordinatePosArr[checkIndexX(indexX)];
 		}
 		return 'The rover moved forward. It is still facing ' + pos + ".";	
 	} else if (x == "B") {
-		//Rover moves backward. Changes coordinate.
 		if (pos == "N") {
-			cordY -= 1;
+			indexY--;
+			cordY = YcoordinatePosArr[checkIndexY(indexY)];
 		} else if (pos == "S") {
-			cordY += 1;
+			indexY++;
+			cordY = YcoordinatePosArr[checkIndexY(indexY)];
 		} else if (pos == "E") {
-			cordX -= 1;
+			indexX--;
+			cordX = XcoordinatePosArr[checkIndexX(indexX)];
 		} else if (pos == "W") {
-			cordX += 1;
+			indexX++;
+			cordX = XcoordinatePosArr[checkIndexX(indexX)];
 		}
 		return 'The rover moved backward. It is still facing ' + pos + ".";
 	}
 }
 
 //Moves the position of the rover according to the array.
-function roverGo() {
-	console.log("Your commands are: " + commandArray);
+function roverGo(){
+	if (commandArray.length > 0) {
+		console.log("Your commands are: " + commandArray);
+		console.log("Launching rover...");
 		for (var i = 0; i <= commandArray.length - 1; i++) {
-			console.log(analyzeCommand(commandArray[i]));	
-				console.log(roverPosition(cordX,cordY));
-		}	
+			console.log(analyzeCommand(commandArray[i]));
+			console.log("It's coordinates are: " + roverPosition(cordX,cordY));
+			console.log("...");
+		}
+	} else {
+		console.log("The rover was given no commands. Cancelling launch...");
+	}
+	prompt.close();
 }
 
 roverStart();
